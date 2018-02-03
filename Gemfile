f35c2c3470331980801696f62d2107c0d4861e15
mgmilton <matt.milton@gmail.com>
2018-02-03 11:32:43 -0700

    adds edit display

--- Gemfile
+++ Gemfile
@@ -18,7 +18,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'pry'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -40,6 +40,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'rspec-rails'
+  gem 'launchy'
   gem 'shoulda-matchers', '~> 3.1'
 end
 
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -25,15 +25,26 @@ class JobsController < ApplicationController
   end
 
   def edit
-    # implement on your own!
+    @job = Job.find(params[:id])
   end
 
   def update
-    # implement on your own!
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    if @job.save
+      flash[:success] = "#{@job.name} updated!"
+      redirect_to job_path(@job)
+    else
+      render :edit
+    end
   end
 
   def destroy
-    # implement on your own!
+    job = Job.find(params[:id])
+    job.destroy
+
+    flash[:success] = "#{job.name} was successfully deleted"
+    redirect_to jobs_path
   end
 
   private
