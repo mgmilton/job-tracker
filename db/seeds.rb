a3b25ab77d09a9e68ae19a7b2e0d5b626fbb1a2b
mgmilton <matt.milton@gmail.com>
2018-02-03 14:48:48 -0700

    adds category index page and categories in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
 Company.destroy_all
 Job.destroy_all
+Category.destroy_all
 
 COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
 JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
 CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+CATEGORIES = ["Web Development", "Finance", "Education", "Non-Profit"]
 
+CATEGORIES.each do |title|
+  Category.create!(title: title)
+end
 
 COMPANIES.each do |name|
   company = Company.create!(name: name)
   puts "Created #{company.name}"
   10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: 1)
+    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category: Category.all.sample)
     puts "  Created #{company.jobs[num].title}"
   end
 end
