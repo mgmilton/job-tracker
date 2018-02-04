082ea96746e2abf3fd20bc32141af7e074102054
mgmilton <matt.milton@gmail.com>
2018-02-03 18:47:32 -0700

    adds category functionality

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe "User creates a new job" do
   scenario "a user can create a new job" do
-    company = Company.create!(name: "ESPN")
     category = Category.create!(title: "Sports!")
+    company = Company.create!(name: "ESPN")
     visit new_company_job_path(company)
 
     fill_in "job[title]", with: "Developer"
@@ -11,14 +11,12 @@ describe "User creates a new job" do
     fill_in "job[level_of_interest]", with: 80
     fill_in "job[city]", with: "Denver"
     select "Sports!", from: "job[category_id]"
-
     click_button "Create"
 
-    expect(current_path).to eq("/companies/#{company.id}/jobs")
-
-    # expect(page).to have_content("ESPN")
-    # expect(page).to have_content("Developer")
-    # expect(page).to have_content("80")
-    # expect(page).to have_content("Denver")
+    expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
+    expect(page).to have_content("ESPN")
+    expect(page).to have_content("Developer")
+    expect(page).to have_content("80")
+    expect(page).to have_content("Denver")
   end
 end
