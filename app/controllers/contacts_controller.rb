04613800fa20289786421d374bef92854d4be827
mgmilton <matt.milton@gmail.com>
2018-02-05 18:52:16 -0700

    adds contact display to company show and updates tests

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,3 +1,13 @@
 class ContactsController < ApplicationController
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.company_id = params[:company_id]
+    @contact.save
+    redirect_to company_jobs_path(@contact.company)
+  end
 
+  private
+  def contact_params
+    params.require(:contact).permit(:full_name, :position, :email)
+  end
 end
