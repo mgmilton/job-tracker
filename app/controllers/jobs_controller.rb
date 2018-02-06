2432fbc4addfee891b338ca6aa1da13173f83958
mgmilton <matt.milton@gmail.com>
2018-02-05 20:04:17 -0700

    adds sorting by city and interest

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,18 +1,16 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :destroy, :edit, :update]
+  before_action :set_company, only: [:new, :create, :edit]
 
   def index
-    @company = Company.find(params[:company_id])
-    @jobs = @company.jobs
+    @jobs = sort_jobs
   end
 
   def new
-    @company = Company.find(params[:company_id])
     @job = Job.new
   end
 
   def create
-    @company = Company.find(params[:company_id])
     @job = @company.jobs.new(job_params)
     if @job.save
       flash[:success] = "You created #{@job.title} at #{@company.name}"
@@ -28,7 +26,7 @@ class JobsController < ApplicationController
   end
 
   def edit
-    @company = @job.company
+
   end
 
   def update
@@ -59,4 +57,18 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
+
+  def sort_jobs
+    if params[:sort] == "location"
+      jobs = Job.order(:city)
+    elsif params[:sort] == "interest"
+      jobs = Job.order(level_of_interest: :desc)
+    else
+      jobs = Job.all
+    end
+    jobs
+  end
 end
