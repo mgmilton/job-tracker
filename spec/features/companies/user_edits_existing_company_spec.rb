f4a6b202566438d5e6135c22d9f1802409fa7909
mgmilton <matt.milton@gmail.com>
2018-02-06 17:25:04 -0700

    updates feature test

--- spec/features/companies/user_edits_existing_company_spec.rb
+++ spec/features/companies/user_edits_existing_company_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "User edits an existing company" do
   scenario "a user can edit a company" do
-    company = Company.create!(name: "ESPN")
+    company = Company.create!(name: "REI")
     visit edit_company_path(company)
 
     fill_in "company[name]", with: "EA Sports"
@@ -10,6 +10,6 @@ describe "User edits an existing company" do
 
     expect(current_path).to eq("/companies/#{company.id}")
     expect(page).to have_content("EA Sports")
-    expect(page).to_not have_content("ESPN")
+    expect(page).to_not have_content("REI")
   end
 end
